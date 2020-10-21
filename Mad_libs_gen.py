3242152bcf824e9d35a708124974593ec82b3ec4
Chrisgarlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-10-21 10:06:42 +0100

    Create Mad_libs_gen.py

---
+++ Mad_libs_gen.py
@@ -0,0 +1,12 @@
+# USER INPUTS NOUN, ADJECTIVE VERB ETC
+
+noun = input("Please enter a noun: ")
+pronoun = input("Please enter a pronoun: ")
+name = input("Please give the noun a name: ")
+adjective = input("Please enter an adjective: ")
+verb = input("Please enter a verb: ")
+
+# ADDS IT INTO A SENTENCE
+
+print(f"There once was a {noun} called {name}.")
+print(f"{pronoun} always {verb} really {adjective} in the mornings.")
