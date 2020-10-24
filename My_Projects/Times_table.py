7fb02185685cdc074856336d366b85131d6565cc
Chrisgarlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-10-24 10:58:21 +0100

    Update Times_table.py

--- My_Projects/Times_table.py
+++ My_Projects/Times_table.py
@@ -1,6 +1,9 @@
+n = int(input("Please enter a number: "))
+
+
 def timestable(n):
     '''
-    Function times each iteration of 1-9 by n
+    Function times each iteration of 1-9 by n (user's input)
     '''
     table = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
@@ -9,4 +12,4 @@ def timestable(n):
         print(nums)
 
 
-timestable(9)
+timestable(n)
