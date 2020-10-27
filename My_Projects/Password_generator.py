13207a679d3dc101c1f525701b8d19c77fb9c5b0
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-10-27 09:55:02 +0000

    Create Password_generator.py

---
+++ My_Projects/Password_generator.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+# Empty list
+password = []
+
+# Convert uppercase alphabet into a list
+uppercase_string = string.ascii_uppercase
+uppercase = list(uppercase_string)
+
+# Convert lowercase alphabet into a list
+lowercase_string = string.ascii_lowercase
+lowercase = list(lowercase_string)
+
+# Clearing the list - gives new password everytime this is run
+password *= 0
+
+# Generate a 12 character password and add to empty list
+while len(password) < 12:
+    password.append(random.choices(uppercase)) or password.append(random.choices(lowercase)) or password.append(random.randint(0, 9))
+
