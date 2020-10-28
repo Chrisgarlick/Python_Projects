8c48595a5f52acc0b2af71a99ba25c1e16a55399
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-10-28 20:15:17 +0000

    Update Mean_median_mode.py

--- My_Projects/Mean_median_mode.py
+++ My_Projects/Mean_median_mode.py
@@ -1,17 +1,20 @@
-lst = [1, 2, 3, 4, 21, 1, 2, 5, 7, 1, 23, 7, 8, 9, 6, 4, 7, 8]
+import math
+
+lst = [1, 4, 5, 67, 2, 3, 5,6 ,1 , 23, 4, 6, 7, 1,2 ,2 ,234 , 3, 32,1 ,12 ,2, 2]
 
 # Mean - Add all up and divide by length
-total = sum(lst)
-amount = len(lst)
-
-mean = total / amount 
-
+def mean(lst):
+    total = sum(lst)
+    average = total / len(lst)
+    return average
 
 # Median - Sort and find middle number
-arranged = lst.sort()
-amount = len(lst)
-median = lst[9]
-
+def median(lst):
+    lst.sort()
+    length = len(lst)
+    mid = length / 2
+    mid = math.ceil(mid)
+    return lst[mid]
 
 # Mode - most occuring number - Without using the counter module
 mode = [[x, lst.count(x)] for x in set(lst)]
