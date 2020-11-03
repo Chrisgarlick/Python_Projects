e7e8c66ac8ac59dd68a7d7b88bede863b9d6c46f
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-03 14:07:37 +0000

    Update Simple_Calc_guess.py

--- My_Projects/Simple_Calc_guess.py
+++ My_Projects/Simple_Calc_guess.py
@@ -1,6 +1,7 @@
 import random
 
 guessed = 0
+score = 0
 
 num1 = random.randint(1, 9)
 num2 = random.randint(1, 9)
@@ -23,21 +24,24 @@ def division(num1, num2):
 
 user = input("Are you ready to play? Enter y or n")
 
-if user == 'y':
-    print(f"The return of the calculation is {randgen()}")
-    show = input("Would you like to know the first number? Enter y or n: ")
-    if show == 'y':
-        print(num1)
-        guess = int(input("Please guess a number: "))
-        guessed += 1
+while score < 3:
+    if user == 'y':
+        print(f"\nThe return of the calculation is {randgen()}")
+        show = input("Would you like to know the first number? Enter y or n: ")
+        if show == 'y':
+            print(num1)
+            guess = int(input("Please guess a number: "))
+            guessed += 1
+        else:
+            guess = int(input("Please guess a number: "))
+            guessed += 1
+        if guess == num1 or guess == num2:
+            print(f"Congrats, it took {guessed} attempt to get it correct!")
+            score += 1
+            print(f"You have {score} correct so far!")
+        else:
+            print("Please try again")
+    elif user == 'n':
+        print("Thanks, have a nice day!")
     else:
-        guess = int(input("Please guess a number: "))
-        guessed += 1
-    if guess == num1 or guess == num2:
-        print(f"Congrats, it took {guessed} attempt to get it correct!")
-    else:
-        print("Please try again")
-elif user == 'n':
-    print("Thanks, have a nice day!")
-else:
-    print("Neither y nor n has been chosen")
+        print("Neither y nor n had been chosen")
