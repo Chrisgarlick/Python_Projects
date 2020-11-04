2baa383d505e50ae4e1ecd26e4db3fbe8f97da08
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-04 15:57:32 +0000

    Update Countdown_timer.py

--- My_Projects/Countdown_timer.py
+++ My_Projects/Countdown_timer.py
@@ -1,12 +1,18 @@
 import time
 
-t = int(input("Please enter how long would you like to start a timer for in seconds: "))
+t = input("Please enter how long would you like to start a timer for in seconds: ")
 
-def countdown(t):
-    while t > 0:
-        print(t)
-        t -= 1
+def countdown(timer):
+    while timer > 0:
+        print(timer)
+        timer -= 1
         time.sleep(1)
     print("You have run out of time!")
     
-countdown(t)
+while not t.isdigit():
+    print("Please enter a valid integer to countdown from: ")
+    t = input("Please enter how long would you like to start a timer for in seconds: ")
+timer = int(t)
+    
+    
+countdown(timer)
