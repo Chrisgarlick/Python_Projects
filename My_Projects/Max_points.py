4372e36f8fb601c1d003e241ad91f2183b659cbe
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-10 20:59:59 +0000

    Create Max_points.py

---
+++ My_Projects/Max_points.py
@@ -0,0 +1,16 @@
+def prem_max_points():
+    teams = 19
+    points = 3
+    games = teams * 2
+    max_points = games * points
+    return max_points
+
+def champ_max_points():
+    teams = 23
+    points = 3
+    games = teams * 2
+    max_points = games * points
+    return max_points
+   
+prem_max_points()
+champ_max_points()
