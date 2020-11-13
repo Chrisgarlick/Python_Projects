60f868ead4198ec725ea23487880849c27f9a7ac
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-13 17:14:40 +0000

    Update Odd_Even_5.py

--- My_Projects/Odd_Even_5.py
+++ My_Projects/Odd_Even_5.py
@@ -1,16 +1,32 @@
-lst = range(0,100)
+a = int(input("Please enter a number to start with: "))
+b = int(input("Please enter a number to end with: "))
+list_nums = list(range(a, b))
 
-def even_nums(lst):
-    '''
-    If a number is divisible by 5, stated. Otherwise stated if Odd or Even
-    '''
-    for item in lst:
-        if item % 5 == 0:
-            print("Divisible by 5!")
-        elif item % 2 == 0:
-            print("This is EVEN!")
-        else:
-            print("This is ODD!")
-            
-            
-even_nums(lst)
+
+class Numbers():
+    
+    def __init__():
+        print("Numbers object created")
+        
+    def odd_nums(list_nums):
+        '''
+        If a number is odd, print odd
+        '''
+        for item in list_nums:
+            if item % 2 == 1:
+                print(f"This is odd {item}")
+    
+    def even_nums(list_nums):
+        '''
+        If a number is divisible by 5, stated. Otherwise stated if Even
+        '''
+        for item in list_nums:
+            if item % 5 == 0:
+                print(f"Divisible by 5!: {item}")
+            elif item % 2 == 0:
+                print(f"This is EVEN: {item}")
+        print(f"{list_nums[-1]}")
+
+even = Numbers
+even.even_nums(list_nums)
+even.odd_nums(list_nums)
