8820e3713be1f3b00a2c489c7fc18074d78b5ce5
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-14 17:47:20 +0000

    Create Pyramid_pattern.py

---
+++ My_Projects/Pyramid_pattern.py
@@ -0,0 +1,14 @@
+def pattern(n):
+    '''
+    Creates a Pyramid Pattern through for loops
+    '''
+    k = 2 * n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print(" ")
+        
+pattern(6)
