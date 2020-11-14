f200a5ef057afeb7a9f165d9de9848a11446f5be
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-14 17:52:10 +0000

    Create Pyramid_rev_pattern.py

---
+++ My_Projects/Pyramid_rev_pattern.py
@@ -0,0 +1,14 @@
+def pattern(n):
+    '''
+    Creates a reversed Pyramid pattern using for loops
+    '''
+    k = 2 * n - 2
+    for i in range(n, -1, -1):
+        for j in range(k, 0, -1):
+            print(end=" ")
+        k = k + 1
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print(" ")
+
+pattern(6)
