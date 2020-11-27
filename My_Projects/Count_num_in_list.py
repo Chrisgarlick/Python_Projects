3b2d1a67eede35cb4fb021087b259c2333d18cf4
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-27 20:25:56 +0000

    Create Count_num_in_list.py

---
+++ My_Projects/Count_num_in_list.py
@@ -0,0 +1,20 @@
+def count(lst, x):
+    """
+    Iterates through a list, if a number is equals to the number to be
+    checked the count increases
+    """
+    count = 0
+    for num in lst:
+        if (num == x):
+            count += 1
+    return count
+
+num_lst = []
+
+while len(num_lst) < 10:
+    numbers = int(input("Please enter a number to add to the list: "))
+    num_lst.append(numbers)
+
+x = int(input("Please enter a number to count: "))
+
+print(f"{x} has occured {count(num_lst, x)} times")
