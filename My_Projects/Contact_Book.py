d54ed69461a05ae65e467982c6381682fcff7ae3
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-11-30 19:44:26 +0000

    Create Contact_Book.py

---
+++ My_Projects/Contact_Book.py
@@ -0,0 +1,17 @@
+contact = {}
+
+finish = "\nPlease enter a contact"
+finish += "\nType 'quit' to finish"
+
+while finish != 'quit':
+    name = input("Please enter a name: ")
+    address = input("Please enter an address: ")
+    postcode = input("Please enter a postcode: ")
+    number = input("Please enter a number: ")
+
+    contact[name] = {name, address, postcode, number}
+    
+    finish = input("Would you like to add another? Type quit to finish")
+
+
+print(contact)
