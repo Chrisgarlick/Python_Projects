33fd621010ed9cc89bbb352ed1da5d8602d40e46
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-12-22 15:05:49 +0000

    Update Bubble_Sort.py

--- My_Projects/Bubble_Sort.py
+++ My_Projects/Bubble_Sort.py
@@ -1,4 +1,4 @@
-def bubblesort(list):
+def bubble_sort(list):
     for i in range(len(list) - 1, 0, -1):
         for x in range(i):
             if list[x] > list[x + 1]:
@@ -8,4 +8,4 @@ def bubblesort(list):
     return list
 
 lst = [5, 6, 2, 3, 8, 7, 1, 4, 9]
-print(bubblesort(lst))
+print(bubble_sort(lst))
