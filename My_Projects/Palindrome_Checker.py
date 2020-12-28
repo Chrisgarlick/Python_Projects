661385adcc5f01335daca72b72eef7ae553c28d1
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-12-28 10:45:14 +0000

    Update Palindrome_Checker.py

--- My_Projects/Palindrome_Checker.py
+++ My_Projects/Palindrome_Checker.py
@@ -1,6 +1,16 @@
-user = input("Please enter a word you would like to check: ")
-
-if user.lower() == user.lower()[::-1]:
-    print("That's a palindrome!")
-else:
-    print("That words not a palindrome!")
+def palindrome(word):
+    if word == word[::-1]:
+        print(f"{word} is a palindrome!")
+    else: 
+        print(f"{word} is not a palindrome, try again!")
+        
+def ready():
+    start = input("Would you like to start? Type 'quit' to end: ")
+    if start == 'quit':
+        return False
+    else:
+        return True
+    
+while ready():
+    go = input("Please enter a word: ")
+    palindrome(go)
