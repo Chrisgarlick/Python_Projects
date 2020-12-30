d1d559d8864a4b757e25c7a23efe70621e85f722
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-12-30 03:05:40 +0000

    Update Fibonacci_to_n.py

--- My_Projects/Fibonacci_to_n.py
+++ My_Projects/Fibonacci_to_n.py
@@ -1,16 +1,45 @@
 def fib(n):
     '''
-    Generate fibonacci sequence to inputted N range
+    Generate fibonacci sequence to inputted N range,
+    while failing silently if any issues occur
     '''
     a = 1
     b = 1
     fib_seq = []
-    
-    for i in range(n):
-        fib_seq.append(a)
-        a, b = b, a+b
-    
-    return fib_seq
+    try:
+        for i in range(n):
+            fib_seq.append(a)
+            a, b = b, a+b
+    except:
+        pass
+    else:
+        return fib_seq
 
+def to_n():
+    '''
+    Ask User for a number to generate fibonacci sequence
+    to, if user inputs something that's not a number
+    it handles the error
+    '''
+    try:
+        user = int(input("Please enter a number for Fibonacci Sequence: "))
+    except:
+        print("That's not a number!")
+    else:
+        return user
+    
+def start():
+    '''
+    If user enters quit it returns false, otherwise it
+    will return True
+    '''
+    ready = input("Are you ready? Type 'quit' to finish: ")
+    if ready == 'quit':
+        return False
+    else: 
+        return True
 
-fib(15)
+while start():
+    num = to_n()
+    fibonacci = fib(num)
+    print(fibonacci)
