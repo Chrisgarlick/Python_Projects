e0c6d1748678994332c0c28611d8cbb7fa8af815
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2021-01-06 13:20:02 +0000

    Update Calculator.py

--- My_Projects/Calculator.py
+++ My_Projects/Calculator.py
@@ -1,30 +1,58 @@
-num1 = int(input("Please enter a number: "))
-num2 = int(input("Please enter another number: "))
-operator = int(input("Please choose a math operator:\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n"))
+def numberOne():
+    num1 = int(input("Please enter a number: "))
+    return num1
+
+def numberTwo():
+    num2 = int(input("Please enter a number: "))
+    return num2 
+
+def operator():
+    try:
+        op = int(input("""Please choose a math operator: 
+                        \n1) Add\n2) Subtract\n3) Multiply\n4) Divide \n"""))
+    except:
+        print("Please choose a valid number: ")
+    finally: 
+        return op
+
+def retry():
+    redo = input("Would you like to use the calculator? Type 'quit' to finish: ")
+
+    if redo == 'quit':
+        return False
+    else:
+        return True
 
 def add(num1, num2):
-    return num1 + num2
+    result = num1 + num2
+    print(f"Your answer is {result}")
 
 def subtract(num1, num2):
-    return num1 - num2
+    result = num1 - num2
+    print(f"Your answer is {result}")
 
 def multiply(num1, num2):
-    return num1 * num2
+    result = num1 * num2
+    print(f"Your answer is {result}")
 
 def divide(num1, num2):
-    return num1 / num2
+    try:
+        result = num1 / num2
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    finally:
+        print(f"Your answer is {result}")
 
-if operator == 1:
-    result = add(num1, num2)
-    print(f"Congrats, your answer is {result}")
-elif operator == 2:
-    result = subtract(num1, num2)
-    print(f"Congrats, your answer is {result}")
-elif operator == 3:
-    result = multiply(num1, num2)
-    print(f"Congrats, your answer is {result}")
-elif operator == 4:
-    result = divide(num1, num2)
-    print(f"Congrats, your answer is {result}")
-else:
-    print("Please ensure you have inputted the correct numbers or operators")
+while retry():
+    num1 = numberOne()
+    num2 = numberTwo()
+    if operator() == 1:
+        add(num1, num2)
+    elif operator() == 2:
+        subtract(num1, num2)
+    elif operator() == 3:
+        multiply(num1, num2)
+    elif operator() == 4:
+        divide(num1, num2)
+    else:
+        print("Please ensure you have inputted correct numbers and operator")
