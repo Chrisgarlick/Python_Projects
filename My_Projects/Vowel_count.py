281dc95a39f8833156e07c0b50ef001e3ca43993
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2021-01-07 02:28:30 +0000

    Update Vowel_count.py

--- My_Projects/Vowel_count.py
+++ My_Projects/Vowel_count.py
@@ -1,23 +1,12 @@
 vowels = 'aeiou'
-count = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u':0}
+count = {vowel: 0 for vowel in vowels}
 
-vow_string = input("Please enter a sentence")
+vow_string = input("Please enter a sentence: ")
 
 for vowel in vow_string:
-    if vowel in vowels:
-        if vowel.lower() == 'a':
-            count['a'] += 1
-        elif vowel.lower() == 'e':
-            count['e'] += 1
-        elif vowel.lower() == 'i':
-            count['i'] += 1
-        elif vowel.lower() == 'o':
-            count['o'] += 1
-        elif vowel.lower() == 'u':
-            count['u'] += 1
-            
-print(f"There is {count['a']} A's in this sentence")
-print(f"There is {count['e']} E's in this sentence")
-print(f"There is {count['i']} I's in this sentence")
-print(f"There is {count['o']} O's in this sentence")
-print(f"There is {count['u']} U's in this sentence")
+    for v in vowels:
+        if vowel.lower() == v:
+            count[v] += 1
+
+for vowel in count:
+    print(f"There are {count[vowel]}{vowel.upper()}'s in this sentence.")
