d7db4f5710f4b87d23b4b9292d8c954b4a9a8a0b
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2021-01-25 21:47:33 +0000

    Update Dice_simulator.py

--- My_Projects/Dice_simulator.py
+++ My_Projects/Dice_simulator.py
@@ -1,20 +1,31 @@
-import random 
+import random
 
-def dice():
-    '''
-    Create two dice which randomize a number between 1 and 6
-    '''
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1, 6)
-    return dice1 + dice2
+class Die():
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def roll(self):
+        result = self.num1 + self.num2
+        print(f"You have just rolled {self.num1} and {self.num2}")
+        if result == 12:
+            print(f"Double 6! You just rolled {result}")
+        elif result >= 10:
+            print(f"High Roll! You just rolled {result}")
+        elif result >= 6:
+            print(f"Big roll! You just rolled {result}")
+        else:
+            print(f"Not amazing, you just rolled {result}")
 
-two_dice = dice()
+def retry():
+    redo = input("Would you like to roll? Type 'quit' to end: ")
+    if redo == 'quit':
+        return False
+    else:
+        return True
 
-if two_dice == 12:
-    print(f"DOUBLE 6!! You've just rolled a {two_dice}")
-elif two_dice >= 10:
-    print(f"High roller! You just rolled a {two_dice}")
-elif two_dice >= 6:
-    print(f"Big roll, you just rolled a {two_dice}")
-else:
-    print(f"Not amazing, but you rolled a {two_dice}")
+while retry():
+    num1 = random.randint(1, 6)
+    num2 = random.randint(1, 6)
+    dice = Die(num1, num2)
+    dice.roll()
